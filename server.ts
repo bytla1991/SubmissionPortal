b5b381dfabef3319c99ab7fbc1ddb8de8eaa4a58
bytla1991 <bhargavajashwanth@gmail.com>
2024-02-29 18:17:29 -0600

    test

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ export function app(): express.Express {
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
-
+// This is the test file
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
